5354091050192ee637e64c21b352d01075c69fbd
wkflanders <wflan@seas.upenn.edu>
2025-10-15 14:11:26 -0400

    fix: cleanup boilerplate code + configs

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@lattic/aeslint-config/react";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
